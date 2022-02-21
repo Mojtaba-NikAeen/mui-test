b488298c275a65d4a5cadbef1f1f85db25d9f2ff
mojtaba <batista_shahi@yahoo.com>
2022-02-21 19:17:42 +0330

    added responsive fontsize for h4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,25 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+const theme = createTheme()
+
+theme.typography.h4 = {
+  [theme.breakpoints.up('md')]: {
+    fontSize: '2.5rem'
+  },
+  [theme.breakpoints.down('md')]: {
+    fontSize: '1.6rem'
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
